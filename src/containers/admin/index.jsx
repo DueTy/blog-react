21a4c4063893f2b8df06059d8a70ebdb266da0ac
DueTy <420770824@qq.com>
2019-03-19 03:15:35 +0800

    update

---
+++ src/containers/admin/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Tabs } from "antd";
+const TabPane = Tabs.TabPane;
+
+import TagEdit from "./tag-edit";
+import ArticleEdit from "./article-edit";
+import { connect } from "react-redux";
+import noPermission from "../../assets/images/no-permission.png";
+import "./index.less";
+
+export default connect(
+    state => state.user
+)(class Admin extends React.Component {    
+    constructor(props) {
+        super(props);
+    }
+    render()  {
+        return (
+            <div className="admin">
+                {
+                    this.props.user ? 
+                        <Tabs tabPosition="left" type="card">
+                            <TabPane tab="文章编辑" key="1">
+                                <ArticleEdit />
+                            </TabPane>
+                            <TabPane tab="属性" key="2">
+                                <TagEdit />
+                            </TabPane>
+                        </Tabs> :
+                        <img src={noPermission} style={{display: "block", margin: "auto"}} />
+                } 
+            </div>
+        );
+    }
+});
